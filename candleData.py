a99bd4b8867dddfbf06579f45bebe3eef43b83eb
Akash Singh <furiousakash111@gmail.com>
2023-09-23 14:28:13 +0530

    Fetching the candle data from Fyers

---
+++ candleData.py
@@ -0,0 +1,24 @@
+from fyers_apiv3 import fyersModel
+import json
+
+#Extracting token
+tokenFile = open('./access_token.json')
+tokenObject = json.load(tokenFile)
+access_token = tokenObject["access_token"]
+
+#Extracting the credentials
+apiCredFile = open('./apiCred.json')
+credObject = json.load(apiCredFile)
+client_id = credObject["client_id"]
+
+fyers = fyersModel.FyersModel(client_id=client_id, token=access_token, is_async=False, log_path="")
+data = {
+    "symbol":"NSE:SBIN-EQ",
+    "resolution":"D",
+    "date_format":"1",
+    "range_from":"2023-09-22",
+    "range_to":"2023-09-22",
+    "cont_flag":"1"
+}
+data = fyers.history(data=data)
+print(data['candles'])
