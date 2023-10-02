d916e6ff495b411a9edfd39bf5b91dbcfd1c560f
Akash Singh <furiousakash111@gmail.com>
2023-10-02 21:23:49 +0530

    Tested for 75 min

--- candleData.py
+++ candleData.py
@@ -14,7 +14,7 @@ client_id = credObject["client_id"]
 fyers = fyersModel.FyersModel(client_id=client_id, token=access_token, is_async=False, log_path="")
 data = {
     "symbol":"NSE:SBIN-EQ",
-    "resolution":"15",
+    "resolution":"75",
     "date_format":"1",
     "range_from":"2023-09-29",
     "range_to":"2023-09-29",
