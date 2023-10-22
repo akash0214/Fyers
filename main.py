30923368cb88e6d34c8427c92f6a2ef80d874132
AKASH SINGH <90954269+akash0214@users.noreply.github.com>
2023-10-23 01:05:50 +0530

    Update main

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ auth_code = input("Enter auth code: ")
 session.set_token(auth_code)
 token_response = session.generate_token()
 
-#Serializing json
+#Serializing json for saving into new file
 token_response_object = json.dumps(token_response, indent=4)
 
 with open('access_token.json', "w") as token:
